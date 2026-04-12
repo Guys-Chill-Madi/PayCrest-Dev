8438537745e4e7488b4e1c27a4ffcbd73b35bd72
Noel Mathews <Noel.Mathews@ust.com>
2026-04-12 13:46:37 +0530

    api-gateway code change

--- api-gateway/server.js
+++ api-gateway/server.js
@@ -1,4 +1,6 @@
 require('dotenv').config();
+const dns = require('dns');
+dns.setDefaultResultOrder('ipv4first');
 const express = require('express');
 const { createProxyMiddleware } = require('http-proxy-middleware');
 const cors = require('cors');
